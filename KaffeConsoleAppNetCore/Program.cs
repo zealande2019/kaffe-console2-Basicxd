b8f408f785d8f01bf51c0c97dcc2c943be2d15ec
basicxd <mikail-fener@hotmail.com>
2019-02-01 13:47:21 +0100

    Nyt

--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,7 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -9,10 +11,29 @@ namespace KaffeConsoleAppNetCore
         {
             Console.WriteLine("Hello World!");
 
-            
+            Cortado cortado = new Cortado();
+            Latte latte = new Latte();
+            FlatWhite flatWhite = new FlatWhite(12);
 
-           
+            List<Imælk> Liste = new List<Imælk>();
 
+            Liste.Add(cortado);
+            Liste.Add(latte);
+
+            foreach (var Item in Liste)
+            {
+                Console.WriteLine("Før Cast" + Item.MlMælk());
+                if (Item is Latte)
+                {
+                    Latte l = Item as Latte;
+                    Console.WriteLine("Latte styrke" + l.Styrke());
+
+                }
+                else if (Item is Cortado)
+                {
+                    Cortado c = Item as Cortado;
+                }  
+            }
         }
     }
 }
